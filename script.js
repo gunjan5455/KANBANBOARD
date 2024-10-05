3888b8e90140045afa8fb5e478f9fa6d4b8ed789
gunjan5455 <155426606+gunjan5455@users.noreply.github.com>
2024-10-05 11:49:03 +0530

    Added

--- script.js
+++ script.js
@@ -7,91 +7,88 @@ const pendingcont = document.querySelector(".pending_cont");
 const hedercolor = document.querySelectorAll(".colors");
 const dele = document.querySelector(".fa-trash");
 const star = document.querySelector(".fa-star");
-const colors = ["pink","blue","purple","green"];
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const colors = ["pink", "blue", "purple", "green"];
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 loadExistingTicketsOnUI();
-function loadExistingTicketsOnUI(){
+function loadExistingTicketsOnUI() {
     const existingTickets = fetchExistingCard();
 
     //display
     displayTickets(existingTickets);
 }
-function displayTickets(tickets){
+function displayTickets(tickets) {
 
 
-    for(let i=0;i<tickets.length;i++){
- 
-     const ticket = tickets[i];
- 
-     console.log(ticket);
- 
-     const {color, content, id, wmonth, wday} =ticket;
- 
-     createticket( color, content, id, wmonth,wday, false);
- 
+    for (let i = 0; i < tickets.length; i++) {
+
+        const ticket = tickets[i];
+        const { color, content, id, wmonth, wday } = ticket;
+
+        createticket(color, content, id, wmonth, wday, false);
+
     }
- 
- }
-addBtn.addEventListener("click",()=>{
+
+}
+addBtn.addEventListener("click", () => {
     model.style.display = "flex";
 })
 
-function removeactive(){
-    for(let i = 0; i < prioritycolor.length;i++){
+function removeactive() {
+    for (let i = 0; i < prioritycolor.length; i++) {
         prioritycolor[i].classList.remove("active");
     }
 }
-for(let i = 0;i < prioritycolor.length;i++){
+for (let i = 0; i < prioritycolor.length; i++) {
     const priorityele = prioritycolor[i];
-    priorityele.addEventListener("click",(e)=>{
+    priorityele.addEventListener("click", (e) => {
         removeactive();
-    // console.log(e.target);
-    e.target.classList.add("active");
-})
+        // console.log(e.target);
+        e.target.classList.add("active");
+    })
 }
 
-model.addEventListener("keypress",(e)=>{
+model.addEventListener("keypress", (e) => {
 
     const key = e.key;
-    if(key != "Enter"){
+    if (key != "Enter") {
         return;
     }
 
     const activecolor = findactive();
-    const  valoftextarea = findtext();
+    const valoftextarea = findtext();
     const ticketId = cardID();
     const mon = currentMonth();
     const dayy = currentDate();
-    createticket(activecolor,valoftextarea,ticketId,mon,dayy,true);
-    textarea.value ="";
+    createticket(activecolor, valoftextarea, ticketId, mon, dayy, true);
+    textarea.value = "";
     removeactive();
     defalutcolor.classList.add("active");
     // console.log(defalutcolor);
     // console.log(activecolor);
     // console.log(valoftextarea);
     // createticket();
-    model.style.display="none";
+    model.style.display = "none";
 
 })
-function findactive(){
-    for(let i = 0; i < prioritycolor.length;i++){
+function findactive() {
+    for (let i = 0; i < prioritycolor.length; i++) {
 
         const acolor = prioritycolor[i];
-        if(acolor.classList.contains("active")){
+        if (acolor.classList.contains("active")) {
             return acolor.classList[1];
         }
 
     }
 }
-function findtext(){
+function findtext() {
     return textarea.value;
 }
 
-function createticket(color,value,ticketId,mon,dayy,addToLocalStorage){
+function createticket(color, value, ticketId, mon, dayy, addToLocalStorage) {
     const ticketcreate = document.createElement("div");
-    ticketcreate.setAttribute("class","card");
-    ticketcreate.innerHTML =   `
+    ticketcreate.setAttribute("class", "card");
+    ticketcreate.innerHTML = `
         <div class="card-colors ${color}"></div>
             <div class="content-box">   
                 <span class="ticket-id">${ticketId}</span>
@@ -111,110 +108,147 @@ function createticket(color,value,ticketId,mon,dayy,addToLocalStorage){
     const contentcard = ticketcreate.querySelector(".card-content");
     const prio_color = ticketcreate.querySelector(".card-colors");
 
-    lockunlock.addEventListener("click",(e)=>lockunlockhemdler(e,contentcard));
-    ticketcreate.addEventListener("click",handlecardclick);
-    prio_color.addEventListener("click",handlepriority);
+    lockunlock.addEventListener("click", (e) => lockunlockhemdler(e, contentcard));
+    ticketcreate.addEventListener("click", handlecardclick);
+    prio_color.addEventListener("click", handlepriority);
 
 
-    
+
 
     pendingcont.appendChild(ticketcreate);
-      
-    if(addToLocalStorage){
-    const newCard ={
-        id:ticketId,
-        color:color,
-        content:value,
-        wmonth:mon,
-        wday:dayy
+
+    if (addToLocalStorage) {
+        const newCard = {
+            id: ticketId,
+            color: color,
+            content: value,
+            wmonth: mon,
+            wday: dayy
+        }
+        addExistingtoLocalStorage(newCard);
     }
-    addExistingtoLocalStorage(newCard);
-}
 }
 
-function addExistingtoLocalStorage(newCard){
+function addExistingtoLocalStorage(newCard) {
     const existingCard = fetchExistingCard();
     existingCard.push(newCard);
     saveCard(existingCard);
 
 }
-function fetchExistingCard(){
+function fetchExistingCard() {
     const existingCard = localStorage.getItem("kanbanCard");
     const existingCardJS = JSON.parse(existingCard);
-    if(existingCardJS == null){
+    if (existingCardJS == null) {
         return [];
     }
     return existingCardJS;
+
 }
-function saveCard(newCard){
+function saveCard(newCard) {
     const newCardJSON = JSON.stringify(newCard);
-    localStorage.setItem("kanbanCard",newCardJSON);
+    localStorage.setItem("kanbanCard", newCardJSON);
 }
-function handlepriority(e){
-    if(star.classList.contains("red")){
-    const existcolor = e.currentTarget.classList[1];
-    const ccolorindex = colors.indexOf(existcolor);
-    
-    
-    const newColorIndex = (ccolorindex + 1)%colors.length;
-    const newColor = colors[newColorIndex];
+function handlepriority(e) {
+    if (star.classList.contains("red")) {
+        console.log(e.currentTarget);
+        const existcolor = e.currentTarget.classList[1];
+        const ccolorindex = colors.indexOf(existcolor);
 
 
-    e.currentTarget.classList.remove(existcolor);
-    e.currentTarget.classList.add(newColor);
+        const newColorIndex = (ccolorindex + 1) % colors.length;
+        const newColor = colors[newColorIndex];
 
+
+        e.currentTarget.classList.remove(existcolor);
+        e.currentTarget.classList.add(newColor);
+
+        let existingCardJS = fetchExistingCard();
+        let parentClass = e.currentTarget.closest(".card");
+        let forCmprTicketid = parentClass.querySelector(".ticket-id").textContent;
+        let index = existingCardJS.findIndex(tic => tic.id === forCmprTicketid);
+        if (index !== -1) {
+            existingCardJS[index].color = newColor;
+            localStorage.setItem('kanbanCard', JSON.stringify(existingCardJS));
+        } else {
+            console.error(`No card found with ID: ${forCmprTicketid}`);
+        }
     }
 }
 
-function handlecardclick(e){
-    if(dele.classList.contains("red")){
+function handlecardclick(e) {
+    if (dele.classList.contains("red")) {
+
         e.currentTarget.remove();
+        console.log(e.currentTarget);
+        let forCmprTicketid = e.currentTarget.querySelector(".ticket-id").textContent;
+        console.log(forCmprTicketid);
+        let existingCardJS = fetchExistingCard();
+        let index = existingCardJS.findIndex(tic => tic.id === forCmprTicketid);
+        if (index !== -1) {
+            existingCardJS.splice(index, 1);
+            localStorage.setItem('kanbanCard', JSON.stringify(existingCardJS));
+
+        }
+
     }
 }
-function lockunlockhemdler(e, contentcard){
+function lockunlockhemdler(e, contentcard) {
     console.log(e.target);
     const isloked = e.target.classList.contains("fa-lock");
 
-    if(isloked){
+    if (isloked) {
         e.target.classList.remove("fa-lock");
         e.target.classList.add("fa-unlock");
-        contentcard.setAttribute("contenteditable","true");
+        contentcard.setAttribute("contenteditable", "true");
 
-    }else{
+    } else {
         e.target.classList.add("fa-lock");
         e.target.classList.remove("fa-unlock");
-        contentcard.setAttribute("contenteditable","false");
+        contentcard.setAttribute("contenteditable", "false");
+        let conText = contentcard.textContent;
+        console.log(conText);
+        let existingCardJS = fetchExistingCard();
+        let parentClass = e.currentTarget.closest(".content-box");
+        let forCmprTicketid = parentClass.querySelector(".ticket-id").textContent;
+        let index = existingCardJS.findIndex(tic => tic.id === forCmprTicketid);
+        if (index !== -1) {
+            existingCardJS[index].content = conText;
+            localStorage.setItem('kanbanCard', JSON.stringify(existingCardJS));
+        } else {
+            console.error(`No card found with ID: ${forCmprTicketid}`);
+        }
     }
 
-} 
-function cardID(){
+
+}
+function cardID() {
     const uid = new ShortUniqueId({ length: 7 });
     const ticketId = uid.rnd();
     return ticketId;
 }
-function currentMonth(){
+function currentMonth() {
     const d = new Date();
     let name = month[d.getMonth()];
     return name;
 }
-function currentDate(){
+function currentDate() {
     const today = new Date();
     const day = String(today.getDate()).padStart(2, '0');
     return day;
 }
-for(let i = 0; i < hedercolor.length;i++){
+for (let i = 0; i < hedercolor.length; i++) {
     const hedercolors = hedercolor[i];
-    hedercolors.addEventListener("click",(e)=>{
+    hedercolors.addEventListener("click", (e) => {
         const cardcolor = e.target.classList[1];
         const allcards = document.querySelectorAll(".card");
-        for(let i = 0;i < allcards.length;i++){
+        for (let i = 0; i < allcards.length; i++) {
             const ticketContainer = allcards[i];
             const ticketColorElement = ticketContainer.querySelector(".card-colors");
             const ticketColor = ticketColorElement.classList[1];
-            if(ticketColor!==cardcolor){
-                ticketContainer.style.display="none";
-            }else{
-                ticketContainer.style.display="block";
+            if (ticketColor !== cardcolor) {
+                ticketContainer.style.display = "none";
+            } else {
+                ticketContainer.style.display = "block";
             }
         }
     })
@@ -228,24 +262,24 @@ for(let i = 0; i < hedercolor.length;i++){
     })
 }
 
-dele.addEventListener("click",()=>{
-    if(dele.classList.contains("red")){
+dele.addEventListener("click", () => {
+    if (dele.classList.contains("red")) {
         dele.classList.remove("red");
         alert("Delete is Deactiveted, Now you can't delete it.");
-    }else{
-    dele.classList.add("red");
-    alert("Delete is activeted, Now you can delete card by clicking on it.");
-    handlecardclick();
+    } else {
+        dele.classList.add("red");
+        alert("Delete is activeted, Now you can delete card by clicking on it.");
+        handlecardclick();
     }
 })
 
-star.addEventListener("click",()=>{
-    if(star.classList.contains("red")){
+star.addEventListener("click", () => {
+    if (star.classList.contains("red")) {
         star.classList.remove("red");
         alert("Priority star is Deactiveted, Now you can't change it.");
-    }else{
-    star.classList.add("red");
-    alert("Priority star is activeted, Now you can change priority of card by clicking on it's color.");
-    handlepriority();
+    } else {
+        star.classList.add("red");
+        alert("Priority star is activeted, Now you can change priority of card by clicking on it's color.");
+        handlepriority();
     }
 })
